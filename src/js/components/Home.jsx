6d4c5ca8f8a08ca15d150f775b895e2c78b1e557
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-04-22 22:02:29 +0000

    falta proptypes y fontawesome

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,38 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+function MyCounter (props){
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div className='mainCounter'>
+		<div className='time'>c</div>
+		<div className='six'>{props.sixNumber}</div>
+		<div className='five'>{props.fifthNumber}</div>
+		<div className='four'>{props.fourthNumber}</div>
+		<div className='three'>{props.thirdNumber}</div>
+		<div className='two'>{props.secondNumber}</div>
+		<div className='one'>{props.firstNumber}</div>
+	  </div>
 	);
-};
+  }
+  MyCounter.propTypes = {
+	sixNumber: PropTypes.number,
+	fifthNumber: PropTypes.number,
+	fourthNumber: PropTypes.number,
+	thirdNumber: PropTypes.number,
+	secondNumber: PropTypes.number,
+	firstNumber: PropTypes.number,
+  };
+  
+  let theCounter = 0;
+  setInterval(function(){
+	const six = Math.floor (theCounter % 100000);
+	const five = Math.floor (theCounter%10000);
+	const four = Math.floor (theCounter%1000);
+	const three = Math.floor (theCounter%100);
+	const two = Math.floor (theCounter%10);
+	const one = Math.floor (theCounter%1);
+	theCounter++;
+  },100000)
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
-
-// index.css'
-import '../styles/index.css'
-
-// components
+import "bootstrap";
+import '../styles/index.css';
+import { faFontAwesome } from '@fortawesome/free-regular-svg-icons';
 import Home from './components/Home';
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+  <Home firstNumber={one} secondNumber={two} thirdNumber={three} fourthNumber={four} fifthNumber={five} sixNumber={six} />
 )
