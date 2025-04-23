d54e00d64497aebb3153e38f2185f31ee9e3820a
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-04-23 19:00:44 +0000

    finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
-
-//create your first component
 function MyCounter (props){
 	return (
 	  <div className='mainCounter'>
-		<div className='time'>c</div>
-		<div className='six'>{props.sixNumber}</div>
-		<div className='five'>{props.fifthNumber}</div>
-		<div className='four'>{props.fourthNumber}</div>
-		<div className='three'>{props.thirdNumber}</div>
-		<div className='two'>{props.secondNumber}</div>
-		<div className='one'>{props.firstNumber}</div>
+		<div className='time'><FontAwesomeIcon icon={faClock} /></div>
+		<div className='sixNum'>{props.sixNumber}</div>
+		<div className='fiveNum'>{props.fifthNumber}</div>
+		<div className='fourNum'>{props.fourthNumber}</div>
+		<div className='threeNum'>{props.thirdNumber}</div>
+		<div className='twoNum'>{props.secondNumber}</div>
+		<div className='oneNum'>{props.firstNumber}</div>
 	  </div>
 	);
   }
@@ -24,15 +25,5 @@ function MyCounter (props){
 	firstNumber: PropTypes.number,
   };
   
-  let theCounter = 0;
-  setInterval(function(){
-	const six = Math.floor (theCounter % 100000);
-	const five = Math.floor (theCounter%10000);
-	const four = Math.floor (theCounter%1000);
-	const three = Math.floor (theCounter%100);
-	const two = Math.floor (theCounter%10);
-	const one = Math.floor (theCounter%1);
-	theCounter++;
-  },100000)
-
-export default Home;
+  
+export default MyCounter;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import '../styles/index.css';
-import { faFontAwesome } from '@fortawesome/free-regular-svg-icons';
-import Home from './components/Home';
+import MyCounter from './components/Home';
+
+let theCounter = 0;
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Home firstNumber={one} secondNumber={two} thirdNumber={three} fourthNumber={four} fifthNumber={five} sixNumber={six} />
-)
+setInterval(function() {
+  let six = Math.floor(theCounter / 100000) % 10;
+  let five = Math.floor(theCounter / 10000) % 10;
+  let four = Math.floor(theCounter / 1000) % 10;
+  let three = Math.floor(theCounter / 100) % 10;
+  let two = Math.floor(theCounter / 10) % 10;
+  let one = theCounter % 10 ;
+
+  console.log(theCounter);
+
+  root.render(
+    <MyCounter
+    firstNumber={one}
+    secondNumber={two}
+    thirdNumber={three}
+    fourthNumber={four}
+    fifthNumber={five}
+    sixNumber={six}
+    />
+  );
+
+  theCounter++;
+}, 1000); 
